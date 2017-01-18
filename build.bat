0b7c0d75c28b2646a057e2395ac818bc38865fd5
dogi <stefan@unterhauser.name>
2017-01-18 07:29:21 -0500

    version 0.12.74

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 7z920.exe
-curl -LOk https://github.com/open-learning-exchange/BeLL-Apps/archive/0.12.73.zip
-7z.exe x 0.12.73.zip
+curl -LOk https://github.com/open-learning-exchange/BeLL-Apps/archive/0.12.74.zip
+7z.exe x 0.12.74.zip
 timeout 15
-move BeLL-Apps-0.12.73 BeLL-Apps
-del /f 0.12.73.zip
+move BeLL-Apps-0.12.74 BeLL-Apps
+del /f 0.12.74.zip
